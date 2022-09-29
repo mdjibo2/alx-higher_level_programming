ba7fd6bcae01114541ecbd4b5db7a5a5fd76d598
mdjibo2 <justpourmestest00@gmail.com>
2022-09-29 01:13:25 +0000

    Correction

--- 0x04-python-more_data_structures/6-main.py
+++ 0x04-python-more_data_structures/6-main.py
@@ -2,3 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
     [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
